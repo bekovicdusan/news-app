9f6722eeb4927e47d19189b052481ab435504e5e
bekovicdusan <bekovicdusan94@gmail.com>
2023-01-02 22:09:07 +0100

    added article component

--- src/components/TheArticle.vue
+++ src/components/TheArticle.vue
@@ -1,13 +1,38 @@
 <template>
-  <div>
-    hello from the article
-  </div>
+  <article class="article">
+    <a class="headline" :href="article.url.toString()" target="blank">
+      <h3>
+        {{ article.title }}
+      </h3>
+    </a>
+    <p class="content">
+      {{ article.content?.split('[')[0] }}
+    </p>
+    <footer class="footer">
+      <p class="timestamp">Published at {{ timestamp }}</p>
+    </footer>
+  </article >
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
+
+import { IArticle } from '@/globals';
 
 export default defineComponent({
-  name: 'TheArticle'
+  name: 'TheArticle',
+  props: {
+    article: {
+      type: Object as PropType<IArticle>,
+      required: true
+    }
+  },
+  setup (props) {
+    const timestamp = new Date(props.article.publishedAt).toLocaleDateString()
+
+    return {
+      timestamp
+    }
+  }
 })
 </script>
