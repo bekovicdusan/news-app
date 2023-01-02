668a252ab024d8d467fe7d64053d081ed2b9c5d5
bekovicdusan <bekovicdusan94@gmail.com>
2023-01-02 22:08:58 +0100

    added articles state

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,16 @@
 import { createStore } from 'vuex'
+import { IArticle } from '@/globals'
 
 export default createStore({
   state: {
+    articles: [] as IArticle[]
   },
   getters: {
   },
   mutations: {
+    populateArticles (state, payload: IArticle[]) {
+      state.articles = [...payload]
+    }
   },
   actions: {
   },
