31efea3465dfe372a6cf9488794a1edbeeb898c9
bekovicdusan <bekovicdusan94@gmail.com>
2023-01-04 18:56:36 +0100

    updated search component

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -1,9 +1,32 @@
 <template>
   <div class="search-component">
     <div class="input-search">
-      <font-awesome-icon class="icon" icon="fa-magnifying-glass"/>
-      <input type="text" placeholder="Search..." />
+      <font-awesome-icon class="icon" icon="fa-magnifying-glass" />
+      <input type="text" placeholder="Search..." @keyup.enter="$emit('user-search', inputVal)" v-model="inputVal"/>
     </div>
-    <button class="primary-btn">Search</button>
+    <category-picker class="category"/>
+    <country-picker class="country"/>
+    <button @click="$emit('user-search', inputVal)" class="primary-btn">Search</button>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+import CountryPicker from './CountryPicker.vue'
+import CategoryPicker from './CategoryPicker.vue'
+
+export default defineComponent({
+  components: {
+    CountryPicker,
+    CategoryPicker
+  },
+  setup() {
+    const inputVal = ref('')
+
+    return {
+      inputVal
+    }
+  }
+})
+</script>
