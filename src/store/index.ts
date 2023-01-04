2e89eca72b6c1ebfc26afa5de411f74a1b6efd74
bekovicdusan <bekovicdusan94@gmail.com>
2023-01-04 18:57:14 +0100

    added new state values and mutations

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,18 +3,24 @@ import { IArticle } from '@/globals'
 
 export default createStore({
   state: {
+    API_KEY: '570b428919d34c2abce2c95a949390fd',
+    selectedCountry: 'us',
+    selectedCategory: 'general',
     articles: [] as IArticle[]
-  },
-  getters: {
   },
   mutations: {
     populateArticles (state, payload: IArticle[]) {
       state.articles = [...payload]
+    },
+    clearArticles (state) {
+      state.articles = []
+    },
+    changeSelectedCountry (state, payload:string) {
+      state.selectedCountry = payload
+    },
+    changeSelectedCategory (state, payload:string) {
+      state.selectedCategory = payload
     }
-  },
-  actions: {
-  },
-  modules: {
   }
 })
  
