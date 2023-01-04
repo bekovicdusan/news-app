2129bc3999506e960335a75dd15875a4c1347ee6
bekovicdusan <bekovicdusan94@gmail.com>
2023-01-04 18:56:14 +0100

    added category and country picker components

---
+++ src/components/CategoryPicker.vue
@@ -0,0 +1,40 @@
+<template>
+  <select class="sort-picker" :value="selectedCategory" @change="handleChange">
+    <option v-for="option in sortOptions" :key="option[0]" :value="option[0]">
+      {{ option[1] }}
+    </option>
+  </select>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+
+import store from '@/store'
+
+export default defineComponent({
+  setup() {
+    const sortOptions = [
+      ['business', 'Business'],
+      ['entertainment', 'Entertainment'],
+      ['general', 'General'],
+      ['health', 'Health'],
+      ['science', 'Science'],
+      ['sports', 'Sports'],
+      ['technology', 'Technology']
+    ]
+
+    const selectedCategory = computed(() => store.state.selectedCategory)
+
+    const handleChange = (event: Event):void => {
+      store.commit('changeSelectedCategory', (event.target as HTMLSelectElement).value)
+    }
+
+    return {
+      sortOptions,
+      selectedCategory,
+
+      handleChange
+    }
+  },
+})
+</script>
---
+++ src/components/CountryPicker.vue
@@ -0,0 +1,40 @@
+<template>
+  <select name="countries" class="country-picker" :value="selectedCountry" @change="handleChange">
+    <option v-for="country in countries" :key="country[0]" :value="country[0]">
+      {{ country[1] }}
+    </option>
+  </select>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue'
+
+import store from '@/store'
+
+export default defineComponent({
+  setup() {
+    const selectedCountry = computed(() => store.state.selectedCountry)
+
+    const countries = ref([
+      ['ae', 'United Arab Emirates'], ['ar', 'Argentina'], ['at', 'Austria'], ['au', 'Australia'], ['be', 'Belgium'], ['bg', 'Bulgaria'], ['br', 'Brazli'], ['ca', 'Canada'],
+      ['ch', 'Switzerland'], ['cn', 'China'], ['co', 'Costa Rica'], ['cu', 'Cuba'], ['cz', 'Czech Republic'], ['de', 'Germany'], ['eg', 'Egypt'], ['fr', 'France'],
+      ['gb', 'Great Britain'], ['gr', 'Greece'], ['hk', 'Hong Kong'], ['hu', 'Hungary'], ['id', 'Indonesia'], ['ie', 'Ireland'], ['il', 'Israel'], ['in', 'India'],
+      ['it', 'Italy'], ['jp', 'Japan'], ['kr', 'Republic of Korea'], ['lt', 'Lithuania'], ['lv', 'Latvia'], ['ma', 'Morocco'], ['mx', 'Mexico'], ['my', 'Malaysia'],
+      ['ng', 'Nigeria'], ['nl', 'Netherlands'], ['no', 'Norway'], ['nz', 'New Zealand'], ['ph', 'Philippines'], ['pl', 'Poland'], ['pt', 'Portugal'], ['ro', 'Romania'],
+      ['rs', 'Serbia'], ['ru', 'Russia'], ['sa', 'Saudi Arabia'], ['se', 'Sweden'], ['sg', 'Singapore'], ['si', 'Slovenia'], ['sk', 'Slovakia'], ['th', 'Thailand'],
+      ['tr', 'Turkey'], ['tw', 'Taiwan'], ['ua', 'Ukraine'], ['us', 'United States'], ['ve', 'Venezuela'], ['za', 'South Africa']
+    ])
+
+    const handleChange = (event: Event):void => {
+      store.commit('changeSelectedCountry', (event.target as HTMLSelectElement).value)
+    }
+
+    return {
+      selectedCountry,
+      countries,
+
+      handleChange
+    }
+  },
+})
+</script>
