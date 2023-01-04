299e4aa96f8c589cacf3f380f2dc5aa6a38ba491
bekovicdusan <bekovicdusan94@gmail.com>
2023-01-04 18:55:34 +0100

    updated app component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,9 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <h1>
+    News App
+  </h1>
   <router-view/>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  setup() {
-    const API_Key = '570b428919d34c2abce2c95a949390fd';
-
-    return {
-      API_Key
-    }
-  },
-})
-</script>
-
 
 <style lang="scss">
 @import './styles/index.scss';
